bcd8397446257a0c8e423f6b86554de36db245d9
miguel Vidal Bravo Jandia <vidalbravojandiamiguel@gmail.com>
2023-01-16 10:21:04 +0100

    Add carousel

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,17 @@
 const slides = [
 	{
 		"image":"Video.jpg",
+        "text":"Echanges Vidéo et partage",
 		"tagLine":"Vous pouvez publier vos vidéos et les partager"
 	},
 	{
 		"image":"collaboratif_petite.jpg",
+        "text":"Publication d'article et partage",
 		"tagLine":"Vous pouvez publier vos articles et entamer des discussions"
 	},
 	{
 		"image":"Document_petite.jpg",
+        "text":"Echanges Documents et partage",
 		"tagLine":"Vous pouvez partager vos documents"
 	}
 ]
@@ -17,4 +20,64 @@ const banner = document.getElementById('#carousel');
 const left = document.querySelector('.arrow_left');
 const right = document.querySelector('.arrow_right');
 const imageElement = document.querySelector('#carousel > img');
-const nomElement = document.querySelector('#carousel > p');
+const nomElement = document.querySelector('#carousel > .tagLine');
+const nomfirstTagLineElement = document.querySelector('#carousel > .text');
+
+i = 0;
+numberOfSlide = slides.length;
+
+left.addEventListener('click', function() {
+    if (i === 0) {
+        i = numberOfSlide - 1;
+    } else {
+        i--
+    }
+    showSlide();
+    console.log(left);
+});
+
+right.addEventListener('click', function() {
+    if (i === numberOfSlide -1) {
+        i = 0;
+    } else {
+        i++
+    }
+    showSlide();
+    console.log(right);
+});
+
+function showSlide() {
+    imageElement.src = `./assets/images/${slides[i].image}`;
+    nomElement.innerHTML = slides[i].tagLine;
+    nomfirstTagLineElement.innerHTML = slides[i].text;
+    console.log(imageElement);
+	console.log(nomElement);
+    console.log(nomfirstTagLineElement);
+}
+showSlide();
+
+function dotsShow () {
+    const dots = document.querySelector('.dots'); 
+    for (let j =0; j < numberOfSlide; j++) {
+    const dot = document.createElement('span');
+    dot.id = 'span' + j;
+    dot.addEventListener('click', function(event) {
+    i = Number(event.target.id.replace('span', ''));
+    showSlide();
+    });
+    dot.classList.add('dot');
+    dots.appendChild(dot);
+    if (i == 0) {
+        dots.children[j].classList.add('.dot-selected');
+    }
+ }
+}
+dotsShow();
+
+function selected() {
+    const dot = document.getElementsByClassName('dot');
+    for (let i = 0; i < dot.length; i++) {
+        dot[i].classList.remove('dot-selected');
+    }
+    dot[i].classList.add('dot-selected');
+}
